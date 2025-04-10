99184b11b7abf4f8651a5bf05fec9fb5e667ebd2
Hirusha Yatagama <hirushayatagama@gmail.com>
2025-04-10 21:24:37 +0530

    Commiting new files

--- Student Management System/src/main/java/com/SMS/DatabaseConnection.java
+++ Student Management System/src/main/java/com/SMS/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 	private static Connection connection;
 	
-	private static final String url = "jdbc:mysql://localhost:3306/";
+	private static final String url = "jdbc:mysql://localhost:3306/StudentManagementDB";
 	private static final String username = "root";
 	private static final String password = "sqlroot";
 	
